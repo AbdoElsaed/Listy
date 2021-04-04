1fdcaf60286719534c31ef90a87113a99f3b0376
ApdoElsaed <abdoelsaed114@gmail.com>
2021-04-04 22:23:07 +0200

    change mongo url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,7 +6,7 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useUnifiedTopology', true);
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Apdo:3146411@cluster0.69pvb.mongodb.net/listyDB?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/listy', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
